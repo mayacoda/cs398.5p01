140d441814c832f46228ad55393724393f96a094
Maya Nedeljkovich <maya.nedeljko@gmail.com>
2018-04-05 12:45:13 +0200

    readme and licensing

--- graphics/bmp-mac.cpp
+++ graphics/bmp-mac.cpp
@@ -11,6 +11,19 @@
 #include <vector>
 
 /**
+ *  Copyright (c) 2005, The EasyBMP Project (http://easybmp.sourceforge.net)
+    All rights reserved.
+
+    Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+
+       1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+       2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+       3. The name of the author may not be used to endorse or promote products derived from this software without specific prior written permission.
+
+    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/**
  * Reads the given number of bytes from the istream into the buffer
  *
  * @param buffer, to store the read bytes
@@ -52,8 +65,8 @@ GLubyte* ReadBitmap(const char* filename, BITMAPINFO** info) {
 
     bool IsBitmap = false;
 
-    if (IsBigEndian() and header.bfType == 16973) IsBitmap     = true;
-    if (not IsBigEndian() and header.bfType == 19778) IsBitmap = true;
+    if (IsBigEndian() && header.bfType == 16973) IsBitmap     = true;
+    if (!IsBigEndian() && header.bfType == 19778) IsBitmap = true;
 
     if (!IsBitmap) {
         fileStream.close();
@@ -189,7 +202,7 @@ ReadBitmap(const char *filename,
   }
 
   /* Now that we have all the header info read in, allocate memory for *
-   * the bitmap and read it in...  
+   * the bitmap and read it in...
    */
   imgsize = (*info)->bmiHeader.biSizeImage;
   // sometimes imagesize is not set in files
